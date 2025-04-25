0428050a7c818dc5dfeda8069bfffc5a7fb856a5
unknown <dimah4254@gmail.com>
2025-04-25 22:54:46 +0500

    added more locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators():
+    SELECT_LANGUAGE = (By.CSS_SELECTOR, "select.form-control")
+    LANGUAGE_GO = (By.CSS_SELECTOR, "#language_selector > button[type='submit']")
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 
 
 class LoginPageLocators(): 
@@ -13,3 +16,12 @@ class LoginPageLocators():
     REGISTRATION_EMAIL_ADRESS = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
+    PRODUCT_STOCK = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.instock.availability")
+    PRODUCT_IMAGE = (By.CSS_SELECTOR, ".item.active img")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, "div.basket-mini.pull-right.hidden-xs")
