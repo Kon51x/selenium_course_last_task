5ea58aae4678e9d6ba4e6e2da4713a3597f176f0
unknown <dimah4254@gmail.com>
2025-04-25 22:56:38 +0500

    update these

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,6 +8,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        # alert = self.browser.switch_to.alert #поддержка алерта, на всякий случай
+        # alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
